cda56544ea06f7ee447a6638f87c7a04bd4a1d26
Fernando Deleg <sistemas@lidenar.com>
2023-09-12 18:30:35 -0500

    Cambio para pagina

--- components/features/product/common/quickview-modal.jsx
+++ components/features/product/common/quickview-modal.jsx
@@ -63,71 +63,7 @@ function Quickview( props ) {
         }, 330 );
     }
 
-    return (
-        <Modal
-            isOpen={ isOpen }
-            contentLabel="QuickView"
-            onRequestClose={ closeQuick }
-            shouldFocusAfterRender={ false }
-            style={ customStyles }
-            className="product product-single row product-popup quickview-modal" id="product-quickview"
-        >
-            <>
-                <div className={ `row p-0 m-0 ${ loaded ? '' : 'd-none' }` } >
-                    <div className="col-md-6">
-                        <div className="product-gallery mb-md-0 pb-0">
-                            <div className="product-label-group">
-                                { product.data.is_new ? <label className="product-label label-new">New</label> : '' }
-                                { product.data.is_top ? <label className="product-label label-top">Top</label> : '' }
-                                {
-                                    product.data.discount > 0 ?
-                                        product.data.variants.length === 0 ?
-                                            <label className="product-label label-sale">{ product.data.discount }% OFF</label>
-                                            : <label className="product-label label-sale">Sale</label>
-                                        : ''
-                                }
-                            </div>
-
-                            <OwlCarousel adClass="product-single-carousel owl-theme owl-nav-inner" options={ mainSlider3 }>
-                                {
-                                    product && product.data && product.data.large_pictures.map( ( item, index ) =>
-                                        <Magnifier
-                                            key={ 'quickview-image-' + index }
-                                            imageSrc={ process.env.NEXT_PUBLIC_ASSET_URI + item.url }
-                                            imageAlt="magnifier"
-                                            largeImageSrc={ process.env.NEXT_PUBLIC_ASSET_URI + item.url }
-                                            dragToMove={ false }
-                                            mouseActivation="hover"
-                                            cursorStyleActive="crosshair"
-                                            className="product-image large-image"
-                                        />
-                                    )
-                                }
-                            </OwlCarousel>
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <DetailOne data={ data } adClass="scrollable pr-3" isNav={ false } />
-                    </div>
-                </div>
-
-                <button title="Close (Esc)" type="button" className="mfp-close p-0" onClick={ closeQuick } ><span>×</span></button>
-            </>
-            {
-                loaded ? '' : <div className="product row p-0 m-0 skeleton-body mfp-product" >
-                    <div className="col-md-6">
-                        <div className="skel-pro-gallery">
-                        </div>
-                    </div>
-
-                    <div className="col-md-6">
-                        <div className="skel-pro-summary"></div>
-                    </div>
-                </div>
-            }
-        </Modal>
-    )
+    
 }
 
 function mapStateToProps( state ) {
